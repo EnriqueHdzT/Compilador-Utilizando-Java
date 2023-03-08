7b7219b03e3b3d90ee100232ae88d247ed12f3d3
Enrique Hdz <hdz_enrique@outlook.com>
2023-03-08 16:37:52 -0600

    Se inicializa el proyecto.
    Se inícialo el proyecto en GitHub utilizando la referencia de código que se nos proporciono. Además se agrego al archivo TipoToken.java una lista mas completa de los tipos de tokens. Igualmente al archivo Scanner.java se le agregaron varias palabras reservadas.

---
+++ TipoToken.java
@@ -0,0 +1,62 @@
+package mx.ipn.escom.compiladores;
+
+public enum TipoToken {
+    // Crear un tipoToken por palabra reservada
+    // Crear un tipoToken: identificador, una cadena y numero
+    // Crear un tipoToken por cada "Signo del lenguaje" (ver clase Scanner)
+
+
+    // Palabras clave:
+    Y, 
+    CLASE, 
+    ADEMAS, 
+    FALSO, 
+    PARA, 
+    FUN, 
+    SI, 
+    NULO, 
+    O, 
+    IMPRIMIR, 
+    RETORNAR, 
+    SUPER, 
+    ESTE, 
+    VERDADERO, 
+    VAR, 
+    MIENTRAS,
+
+    // Identificador
+    IDENTIFICADOR,
+
+    // Cadena
+    CADENA, 
+    
+    // Numero
+    NUMERO,
+
+    // Signos y Simbolos
+
+    PAR_IZ,                 // (
+    PAR_DER,                // )
+    LLAVE_IZ,               // {
+    LLAVE_DER,              // }
+    COMA,                   // ,
+    PUNTO,                  // .
+    PUNTO_COMA,             // ;
+    MENOS,                  // -
+    MAS,                    // +
+    POR,                    // *
+    BARRA,                  // /
+    NIEGA,                  // !
+    DIFERENTE_DE,           // !=
+    ASIGNA,                 // =
+    IGUAL_QUE,              // ==
+    MENOR_QUE,              // <
+    MENOR_O_IGUAL_QUE,      // <=
+    MAYOR_QUE,              // >
+    MAYOR_O_IGUAL_QUE,      // >=
+    LINEA_COMENTARIO,       // //
+    COMENTARIO_LARGO,       // /* */
+
+    // Final de cadena
+    EOF,
+}
