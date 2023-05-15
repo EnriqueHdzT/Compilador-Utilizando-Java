722c106a73fc85b154948b92f6335bc6463c4652
Enrique Hdz <hdz_enrique@outlook.com>
2023-05-14 21:53:11 -0600

    fixed some basic errors

--- Interprete.java
+++ Interprete.java
@@ -1,12 +1,8 @@
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
