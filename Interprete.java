d633a2d027f9c19069f3e068429b4e9f5c34081c
Enrique Hdz <hdz_enrique@outlook.com>
2023-06-04 22:15:37 -0600

    Se corriguieron algunos errores del analizador lexico

--- Interprete.java
+++ Interprete.java
@@ -11,14 +11,14 @@ public class Interprete {
     static boolean existenErrores = false;
 
     public static void main(String[] args) throws IOException {
-        if(args.length > 1) {
+        if (args.length > 1) {
             System.out.println("Uso correcto: interprete [script]");
 
             // Convención defininida en el archivo "system.h" de UNIX
             System.exit(64);
-        } else if(args.length == 1){
+        } else if (args.length == 1) {
             ejecutarArchivo(args[0]);
-        } else{
+        } else {
             ejecutarPrompt();
         }
     }
@@ -27,7 +27,7 @@ public class Interprete {
         File arch_entrada = new File(path);
         Scanner texto = new Scanner(arch_entrada);
         String data = "";
-        while(texto.hasNextLine()){
+        while (texto.hasNextLine()) {
             data += texto.nextLine() + "\n";
         }
         texto.close();
@@ -36,46 +36,50 @@ public class Interprete {
         ejecutar(data);
 
         // Se indica que existe un error
-        if(existenErrores) System.exit(65);
+        if (existenErrores)
+            System.exit(65);
     }
 
-    private static void ejecutarPrompt() throws IOException{
+    private static void ejecutarPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for(;;){
+        for (;;) {
             System.out.print(">>> ");
             String linea = reader.readLine();
             linea = linea.replace(" ", "");
             linea = linea.replace("\t", "");
-            if(linea == null) break; // Presionar Ctrl + D
+            if (linea == null)
+                break; // Presionar Ctrl + D
             ejecutar(linea);
             existenErrores = false;
         }
     }
 
-    private static void ejecutar(String source){
+    private static void ejecutar(String source) {
         Escaneo scanner = new Escaneo(source);
         List<Token> tokens = scanner.scanTokens();
-
-        for(Token token : tokens){
-            System.out.println(token);
-        }
+        /*
+         * for (Token token : tokens) {
+         * System.out.println(token);
+         * }
+         */
+        Parseo parseo = new Parseo(tokens);
+        parseo.parseo();
     }
 
     /*
-    El método error se puede usar desde las distintas clases
-    para reportar los errores:
-    Interprete.error(....);
+     * El método error se puede usar desde las distintas clases
+     * para reportar los errores:
+     * Interprete.error(....);
      */
-    static void error(int linea, String mensaje){
+    static void error(int linea, String mensaje) {
         reportar(linea, "", mensaje);
     }
 
-    private static void reportar(int linea, String donde, String mensaje){
+    private static void reportar(int linea, String donde, String mensaje) {
         System.err.println(
-                "[linea " + linea + "] Error " + donde + ": " + mensaje
-        );
+                "[linea " + linea + "] Error " + donde + ": " + mensaje);
         existenErrores = true;
     }
 }
--- Parseo.java
+++ Parseo.java
@@ -1,15 +1,48 @@
-/* import java.util.List;
+import java.util.List;
 
 public class Parseo {
     private final List<Token> tokens;
 
-    private final Token identificador = new Token(TipoToken.IDENTIFICADOR, "");
-    private final Token select = new Token(TipoToken.SELECT, "select");
-    private final Token from = new Token(TipoToken.FROM, "from");
-    private final Token distinct = new Token(TipoToken.DISTINCT, "distinct");
-    private final Token coma = new Token(TipoToken.COMA, ",");
-    private final Token punto = new Token(TipoToken.PUNTO, ".");
-    private final Token asterisco = new Token(TipoToken.ASTERISCO, "*");
+    private final Token clase = new Token(TipoToken.CLASS, "class");
+    private final Token funcion = new Token(TipoToken.FUNCTION, "fun");
+    private final Token variable = new Token(TipoToken.VARIABLE, "var");
+    private final Token cicloMientras = new Token(TipoToken.WHILE_LOOP, "while");
+    private final Token cicloPara = new Token(TipoToken.FOR_LOOP, "for");
+    private final Token condicionSi = new Token(TipoToken.IF, "if");
+    private final Token condicionDeOtraForma = new Token(TipoToken.ELSE, "else");
+    private final Token condicionO = new Token(TipoToken.OR, "or");
+    private final Token condicionY = new Token(TipoToken.AND, "and");
+    private final Token imprime = new Token(TipoToken.PRINT, "print");
+    private final Token regresa = new Token(TipoToken.RETURN, "return");
+    private final Token verdadero = new Token(TipoToken.TRUE, "true");
+    private final Token falso = new Token(TipoToken.FALSE, "false");
+    private final Token nulo = new Token(TipoToken.NULL, "null");
+    private final Token esto = new Token(TipoToken.THIS, "this");
+    private final Token ssuper = new Token(TipoToken.SUPER, "super");
+
+    private final Token parentesisIzq = new Token(TipoToken.PARENTHESIS_LEFT, "(");
+    private final Token parentesisDer = new Token(TipoToken.PARENTHESIS_RIGHT, ")");
+    private final Token llavesIzq = new Token(TipoToken.BRACKET_LEFT, "{");
+    private final Token llavesDer = new Token(TipoToken.BRACKET_RIGHT, "}");
+    private final Token puntoComa = new Token(TipoToken.SEMICOLON, ";");
+    private final Token igual = new Token(TipoToken.EQUALS, "=");
+    private final Token exclamacion = new Token(TipoToken.EXCLAMATION, "!");
+    private final Token menorQue = new Token(TipoToken.LESS_THAN, "<");
+    private final Token mayorQue = new Token(TipoToken.GREATER_THAN, ">");
+    private final Token menorOIgualQue = new Token(TipoToken.LESS_OR_EQUAL_THAN, "<=");
+    private final Token mayorOIgualQue = new Token(TipoToken.GREATER_OR_EQUAL_THAN, ">=");
+    private final Token diferenteQue = new Token(TipoToken.DIFFERENT_THAN, "!=");
+    private final Token igualQue = new Token(TipoToken.EQUAL_THAN, "==");
+    private final Token menor = new Token(TipoToken.MINUS, "-");
+    private final Token suma = new Token(TipoToken.PLUS, "+");
+    private final Token barra = new Token(TipoToken.SLASH, "/");
+    private final Token asterisco = new Token(TipoToken.ASTERISK, "*");
+    private final Token coma = new Token(TipoToken.COMMA, ",");
+    private final Token punto = new Token(TipoToken.DOT, ".");
+
+    private final Token identificador = new Token(TipoToken.ID, "");
+    private final Token numero = new Token(TipoToken.NUMBER, "");
+    private final Token cadena = new Token(TipoToken.STRING, "");
     private final Token finCadena = new Token(TipoToken.EOF, "");
 
     private int i = 0;
@@ -17,169 +50,175 @@ public class Parseo {
 
     private Token preanalisis;
 
-    public Parseo(List<Token> tokens){
+    public Parseo(List<Token> tokens) {
         this.tokens = tokens;
     }
 
-    public void parseo(){
+    public void parseo() {
         i = 0;
         preanalisis = tokens.get(i);
         Q();
 
-        if(!hayErrores && !preanalisis.equals(finCadena)){
-            System.out.println("Error en la posición " + preanalisis.linea + ". No se esperaba el token " + preanalisis.tipo);
-        }
-        else if(!hayErrores && preanalisis.equals(finCadena)){
+        if (!hayErrores && !preanalisis.equals(finCadena)) {
+            System.out.println(
+                    "Error en la posición " + preanalisis.linea + ". No se esperaba el token " + preanalisis.tipo);
+        } else if (!hayErrores && preanalisis.equals(finCadena)) {
             System.out.println("Consulta válida");
         }
 
-        /*if(!preanalisis.equals(finCadena)){
-            System.out.println("Error en la posición " + preanalisis.posicion + ". No se esperaba el token " + preanalisis.tipo);
-        }else if(!hayErrores){
-            System.out.println("Consulta válida");
-        }*/
-/*    }
+        /*
+         * if(!preanalisis.equals(finCadena)){
+         * System.out.println("Error en la posición " + preanalisis.posicion +
+         * ". No se esperaba el token " + preanalisis.tipo);
+         * }else if(!hayErrores){
+         * System.out.println("Consulta válida");
+         * }
+         */
+    }
 
-    void Q(){
-        if(preanalisis.equals(select)){
+    void Q() {
+        if (preanalisis.equals(select)) {
             coincidir(select);
             D();
             coincidir(from);
             T();
-        }
-        else{
+        } else {
             hayErrores = true;
-            System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba la palabra reservada SELECT.");
+            System.out.println(
+                    "Error en la posición " + preanalisis.linea + ". Se esperaba la palabra reservada SELECT.");
         }
     }
 
-    void D(){
-        if(hayErrores) return;
+    void D() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(distinct)){
+        if (preanalisis.equals(distinct)) {
             coincidir(distinct);
             P();
-        }
-        else if(preanalisis.equals(asterisco) || preanalisis.equals(identificador)){
+        } else if (preanalisis.equals(asterisco) || preanalisis.equals(identificador)) {
             P();
-        }
-        else{
+        } else {
             hayErrores = true;
-            System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba DISTINCT, * o un identificador.");
+            System.out.println(
+                    "Error en la posición " + preanalisis.linea + ". Se esperaba DISTINCT, * o un identificador.");
         }
     }
 
-    void P(){
-        if(hayErrores) return;
+    void P() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(asterisco)){
+        if (preanalisis.equals(asterisco)) {
             coincidir(asterisco);
-        }
-        else if(preanalisis.equals(identificador)){
+        } else if (preanalisis.equals(identificador)) {
             A();
-        }
-        else{
+        } else {
             hayErrores = true;
-            System.out.println("Error en la posición \" + preanalisis.posicion + \". Se esperaba * o un identificador.");
+            System.out
+                    .println("Error en la posición \" + preanalisis.posicion + \". Se esperaba * o un identificador.");
         }
     }
 
-    void A(){
-        if(hayErrores) return;
+    void A() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(identificador)){
+        if (preanalisis.equals(identificador)) {
             A2();
             A1();
-        }
-        else{
+        } else {
             hayErrores = true;
             System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba un identificador.");
         }
     }
 
-    void A1(){
-        if(hayErrores) return;
+    void A1() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(coma)){
+        if (preanalisis.equals(coma)) {
             coincidir(coma);
             A();
         }
     }
 
-    void A2(){
-        if(hayErrores) return;
+    void A2() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(identificador)){
+        if (preanalisis.equals(identificador)) {
             coincidir(identificador);
             A3();
-        }
-        else{
+        } else {
             hayErrores = true;
             System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba un identificador.");
         }
     }
 
-    void A3(){
-        if(hayErrores) return;
+    void A3() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(punto)){
+        if (preanalisis.equals(punto)) {
             coincidir(punto);
             coincidir(identificador);
         }
     }
 
-    void T(){
-        if(hayErrores) return;
+    void T() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(identificador)){
+        if (preanalisis.equals(identificador)) {
             T2();
             T1();
-        }
-        else{
+        } else {
             hayErrores = true;
             System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba un identificador.");
         }
     }
 
-    void T1(){
-        if(hayErrores) return;
+    void T1() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(coma)){
+        if (preanalisis.equals(coma)) {
             coincidir(coma);
             T();
         }
     }
 
-    void T2(){
-        if(hayErrores) return;
+    void T2() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(identificador)){
+        if (preanalisis.equals(identificador)) {
             coincidir(identificador);
             T3();
-        }
-        else{
+        } else {
             hayErrores = true;
             System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba un identificador.");
         }
     }
 
-    void T3(){
-        if(hayErrores) return;
+    void T3() {
+        if (hayErrores)
+            return;
 
-        if(preanalisis.equals(identificador)){
+        if (preanalisis.equals(identificador)) {
             coincidir(identificador);
         }
     }
 
+    void coincidir(Token t) {
+        if (hayErrores)
+            return;
 
-    void coincidir(Token t){
-        if(hayErrores) return;
-
-        if(preanalisis.tipo == t.tipo){
+        if (preanalisis.tipo == t.tipo) {
             i++;
             preanalisis = tokens.get(i);
-        }
-        else{
+        } else {
             hayErrores = true;
             System.out.println("Error en la posición " + preanalisis.linea + ". Se esperaba un  " + t.tipo);
 
@@ -187,4 +226,3 @@ public class Parseo {
     }
 
 }
- */
